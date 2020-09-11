bcb5635870346089317d868a786d5c57088168c5
Ian <41586905+ianbaxter@users.noreply.github.com>
2020-09-11 16:05:48 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import "normalize.css"
+import "./index.css"
+import StoreGrid from "../components/StoreGrid/StoreGrid"
+import Nav from "../components/Nav/Nav"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="app">
+      <Nav />
+      <StoreGrid />
+    </div>
+  )
 }
